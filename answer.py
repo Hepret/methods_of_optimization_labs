330453374b76473951f47abae054a2005ed9cda0
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-01 22:07:45 +0300

    Сделал rename поля в классе answer minimum_point -> point_of_minimum

--- answer.py
+++ answer.py
@@ -1,10 +1,10 @@
 class Answer:
-    def __init__(self, minimum_point: float, minimum_value: float, iterations: int, accuracy: float,
+    def __init__(self, point_of_minimum: float, minimum_value: float, iterations: int, accuracy: float,
                  function_calls: int, localization_segment_length: float):
         self.localization_segment_length = localization_segment_length
         self.function_calls = function_calls
         self.accuracy = accuracy
         self.iterations = iterations
         self.minimum_value = minimum_value
-        self.minimum_point = minimum_point
+        self.point_of_minimum = point_of_minimum
 
