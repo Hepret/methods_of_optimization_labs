15df6568acd60c4273a56eb42c3d2503e71fbfce
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-01 22:16:43 +0300

    Добавил возможность преобразования к строке

--- answer.py
+++ answer.py
@@ -8,3 +8,12 @@ class Answer:
         self.minimum_value = minimum_value
         self.point_of_minimum = point_of_minimum
 
+    def __str__(self):
+
+        return f'''Point of minimum: {self.point_of_minimum}
+Minimum value: {self.minimum_value}
+Total iterations: {self.iterations}
+Total function calls: {self.function_calls}
+Accuracy: {self.accuracy}
+Last localization segment length: {self.localization_segment_length}
+        '''
