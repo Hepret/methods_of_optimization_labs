4df0774c7ecc3cc5305ed1ca140a05a49a73549a
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-01 22:09:37 +0300

    Добавил функцию тестирующую метод деления пополам

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from function import *
+from function import function
+from bisection_method import bisection_method
 
-def main(): 
-    pass
+
+def test_bisection_method():
+    answer = bisection_method(function)
+
+    print(f"Point of minimum: {answer.point_of_minimum}")
+    print(f"Minimum value: {answer.minimum_value}")
+    print(f"Total iterations: {answer.iterations}")
+    print(f"Total function calls: {answer.function_calls}")
+    print(f"Accuracy: {answer.accuracy}")
+    print(f"Last localization segment length: {answer.localization_segment_length}")
+
+
+def main():
+    test_bisection_method()
+
 
 if __name__ == "__main__":
     main()
